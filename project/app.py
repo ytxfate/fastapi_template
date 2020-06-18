5841c42577a9959505a6243d7fe77c96271d1591
hsd <1426482738@qq.com>
2020-06-18 11:33:55 +0800

    [IMP] 添加api文档的展示判断

--- project/app.py
+++ project/app.py
@@ -10,11 +10,13 @@
 from fastapi import FastAPI
 from starlette.middleware.cors import CORSMiddleware
 # User-defined Modules
-from project.config.sys_config import prefix_api_path
+from project.config.sys_config import prefix_api_path, isFormalSystem
 
+# 判断是否展示接口文档
+docs_url = (prefix_api_path + "/docs") if isFormalSystem is False else None
+redoc_url = (prefix_api_path + "/redoc") if isFormalSystem is False else None
 
-app = FastAPI()
-
+app = FastAPI(docs_url=docs_url, redoc_url=redoc_url)
 
 # 跨域处理
 app.add_middleware(
