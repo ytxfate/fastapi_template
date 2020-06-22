c53dd8f47f647abc396c4881e086121d4154eb2d
hsd <1426482738@qq.com>
2020-06-22 15:37:13 +0800

    [IMP] 优化启动

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 #!/usr/bin/env python3
 # -*- coding:utf-8 -*-
-
 '''
 @File :  main.py  
 @Desc :  启动
@@ -12,16 +11,17 @@ import uvicorn
 from project.config.sys_config import app_run_conf, isFormalSystem
 
 
-
-if __name__ == "__main__":
+def main_run():
     if isFormalSystem is True:
         app_run_conf['RELOAD'] = False
         app_run_conf['DEBUG'] = False
-    uvicorn.run(
-        "project.app:app",
-        host=app_run_conf['HOST'],
-        port=app_run_conf['PORT'],
-        reload=app_run_conf['RELOAD'],
-        workers=app_run_conf['WORKERS'],
-        debug=app_run_conf['DEBUG']
-    )
+    uvicorn.run("project.app:app",
+                host=app_run_conf['HOST'],
+                port=app_run_conf['PORT'],
+                reload=app_run_conf['RELOAD'],
+                workers=app_run_conf['WORKERS'],
+                debug=app_run_conf['DEBUG'])
+
+
+if __name__ == "__main__":
+    main_run()
