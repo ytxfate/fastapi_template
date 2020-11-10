781bd11ec854b9d82d2b416587f65edab3538dde
hsd <1426482738@qq.com>
2020-11-10 22:31:56 +0800

    [ADD] 添加系统访问日志记录(默认不开启)

--- project/interceptor/before_req.py
+++ project/interceptor/before_req.py
@@ -16,6 +16,7 @@ from project.utils import resp_code
 from project.utils.comm_ret import comm_ret
 from project.utils.jwt_auth import JWTAuth
 from project.utils.operate_mongodb import OperateMongodb
+# from project.utils.sys_access_log import sys_access_log
 
 
 logger = logging.getLogger("uvicorn")
@@ -23,6 +24,10 @@ _, db_mongo = OperateMongodb().conn_mongodb()
 
 @app.middleware("http")
 async def app_before_request(request: Request, call_next):
+    # 访问日志记录 
+    # (取消注释 [文件头的库导入也需打开] 则生成日志信息,但并不保存,需自行选择存储方式)
+    # await sys_access_log(request)
+    
     # 按要求拦截请求
     path = request.url.path
 
---
+++ project/utils/sys_access_log.py
@@ -0,0 +1,61 @@
+#!/usr/bin/env python3
+# -*- coding:utf-8 -*-
+
+'''
+@File :  sys_access_log.py  
+@Desc :  系统访问日志记录
+'''
+
+# Standard library imports
+import logging
+# Third party imports
+from fastapi import Request
+from starlette.requests import Message
+# Local application imports
+
+
+logger = logging.getLogger("uvicorn")
+
+async def set_body(request: Request, body: bytes):
+    async def receive() -> Message:
+        return {"type": "http.request", "body": body}
+    request._receive = receive
+
+
+# async def get_body(request: Request) -> bytes:
+#     body = await request.body()
+#     await set_body(request, body)
+#     return body
+
+
+async def sys_access_log(request: Request):
+    """系统访问日志记录
+    """
+    _body = await request.body()
+    # 获取真实的 ip (可能存在 nginx 等方式的代理)
+    ip = request.client.host
+    __x_forwarded_for = request.headers.getlist("X-Forwarded-For") or []
+    __x_real_ip = request.headers.getlist("X-Real-Ip") or None
+    if __x_forwarded_for:
+        ip = __x_forwarded_for[0]
+    elif __x_real_ip:
+        ip = __x_real_ip
+
+    req_log_dict = {
+        'uri': request.url.path,
+        'method': request.method,
+        'ip': ip,
+        'url': request.url.path,
+        'query_params': request.query_params._dict,
+        'path_params': request.path_params,
+    }
+    try:
+        # 字符类参数可以进行编码存储
+        req_log_dict['body'] = _body.decode()
+    except Exception as e:
+        logger.error(e)
+        req_log_dict['body'] = f"** It could be a byte file ({e}) **"
+
+    print(req_log_dict)   # 若要记录日志可在此处进行持久化操作
+    
+    await set_body(request, _body)
