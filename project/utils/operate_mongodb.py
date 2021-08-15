f25c4fffb781cc845fc07e69793db37736337ffe
hsd <1426482738@qq.com>
2021-08-15 17:11:45 +0800

    🎈 perf(数据库连接工具): 优化mongo及redis数据库连接模式

--- project/utils/operate_mongodb.py
+++ project/utils/operate_mongodb.py
@@ -8,11 +8,34 @@
 
 # Standard library imports
 import threading
+from collections import namedtuple
+import logging
 # Third party imports
 import pymongo
 # Local application imports
 from project.config.db_config import MONGODB_CONF, MONGODB_CONF_T
 from project.config.sys_config import isFormalSystem
+
+# ============================ 定义 MongoDB 连接配置 =========================== #
+MONGODB_CONF_NT = namedtuple("MONGODB_CONFIG", [
+    "URL",          # 有此项则优先用此项进行数据库连接
+                    # 否则用 HOST 和 PORT 连接
+    "HOST",         # 连接地址
+    "PORT",         # 连接端口
+    "AUTH",         # AUTH 为 True 时需要进行 用户认证
+    "USERNAME",     # 用户名
+    "PASSWORD",     # 密码
+    "DEFAULT_DB"    # 默认数据库
+])
+# 设置默认值
+MONGODB_CONF_NT.__new__.__defaults__ = ("",
+                                        "127.0.0.1",
+                                        27017,
+                                        False,
+                                        "xxx",
+                                        "xxx",
+                                        "xxx")
+# ============================================================================ #
 
 
 class OperateMongodb:
@@ -21,13 +44,17 @@ class OperateMongodb:
     """
     _instance_lock = threading.Lock()
 
-    def __init__(self):
-        # 根据 isFormalSystem 判断连接哪个 mongo 数据库
-        if isFormalSystem:
-            self.MONGO_CONF = MONGODB_CONF
-        else:
-            self.MONGO_CONF = MONGODB_CONF_T
+    conn_mongo, db_mongo = None, None
     
+    def __init__(self, priority_conf: dict={}):
+        """priority_conf 存在则优先使用
+        """
+        # 判断获取那个连接配置
+        tmp_conf = priority_conf or (MONGODB_CONF if isFormalSystem else MONGODB_CONF_T)
+        self.mongodb_conf = MONGODB_CONF_NT(**tmp_conf)
+        logging.info(self.mongodb_conf)
+
+
     def __new__(cls, *args, **kwargs):
         """
         实现单例模式
@@ -37,20 +64,43 @@ class OperateMongodb:
                 if not hasattr(cls, '_instance'):
                     OperateMongodb._instance = super().__new__(cls)
         return OperateMongodb._instance
-    
-    def conn_mongodb(self):
+
+
+    def __conn_mongodb(self):
         """
         连接 MongoDB 数据库
             @return:
                 mongo_connection and mongo_database
         """
-        if 'URL' in self.MONGO_CONF and self.MONGO_CONF['URL'] != '':
-            conn = pymongo.MongoClient(host=self.MONGO_CONF['URL'])
+        if self.mongodb_conf.URL:
+            self.conn_mongo = pymongo.MongoClient(host=self.mongodb_conf.URL)
         else:
-            conn = pymongo.MongoClient(
-                host=self.MONGO_CONF['HOST'], port=self.MONGO_CONF['PORT'])
-        db = conn.get_database(self.MONGO_CONF['DEFAULT_DB'])
-        if self.MONGO_CONF['AUTH'] is True:
-            db.authenticate(
-                self.MONGO_CONF['USERNAME'], self.MONGO_CONF['PASSWORD'])
-        return conn, db
+            self.conn_mongo = pymongo.MongoClient(host=self.mongodb_conf.HOST,
+                                                  port=self.mongodb_conf.PORT)
+        self.db_mongo = self.conn_mongo.get_database(self.mongodb_conf.DEFAULT_DB)
+        if self.mongodb_conf.AUTH is True:
+            self.db_mongo.authenticate(self.mongodb_conf.USERNAME,
+                                       self.mongodb_conf.PASSWORD)
+        logging.info("mongodb connected.")
+
+
+    def get_conn_and_db(self):
+        """获取 mongo_connection 和 mongo_database
+        """
+        if (not self.conn_mongo) or (not self.db_mongo):
+            self.__conn_mongodb()
+        return self.conn_mongo, self.db_mongo
+
+
+    def conn_mongodb(self):
+        """兼容老代码
+        """
+        return self.get_conn_and_db()
+
+
+    def __del__(self):
+        try:
+            self.conn_mongo.close()
+        except:
+            pass
+        logging.info("mongodb closed.")
--- project/utils/operate_redis.py
+++ project/utils/operate_redis.py
@@ -7,6 +7,8 @@
 '''
 
 # Standard library imports
+from collections import namedtuple
+import logging
 import threading
 # Third party imports
 import redis
@@ -15,18 +17,41 @@ from project.config.db_config import REDIS_CONF, REDIS_CONF_T
 from project.config.sys_config import isFormalSystem
 
 
+# ============================= 定义 Redis 连接配置 ============================ #
+REDIS_CONF_NT = namedtuple("Redis_CONFIG", [
+    "HOST",         # 连接地址
+    "PORT",         # 连接端口
+    "AUTH",         # AUTH 为 True 时需要进行 用户认证
+    "PASSWORD",     # 密码
+    "DECODE_RESPONSES", # 是否对查询结果进行编码处理
+    "DEFAULT_DB"    # 默认数据库
+])
+# 设置默认值
+REDIS_CONF_NT.__new__.__defaults__ = ("127.0.0.1",
+                                      6379,
+                                      False,
+                                      "xxx",
+                                      True,
+                                      0)
+# ============================================================================ #
+
+
 class OperateRedis:
     """
     操作 Redis 数据库
     """
     _instance_lock = threading.Lock()
 
-    def __init__(self):
-        # 根据 isFormalSystem 判断连接哪个 redis 数据库
-        if isFormalSystem:
-            self.REDIS_CONFIG = REDIS_CONF
-        else:
-            self.REDIS_CONFIG = REDIS_CONF_T
+    redis_cli = None
+
+    def __init__(self, priority_conf: dict={}):
+        """priority_conf 存在则优先使用
+        """
+        # 判断获取那个连接配置
+        tmp_conf = priority_conf or (REDIS_CONF if isFormalSystem else REDIS_CONF_T)
+        self.redis_conf = REDIS_CONF_NT(**tmp_conf)
+        logging.info(self.redis_conf)
+    
     
     def __new__(cls, *args, **kwargs):
         if not hasattr(cls, '_instance'):
@@ -34,25 +59,46 @@ class OperateRedis:
                 if not hasattr(cls, '_instance'):
                     OperateRedis._instance = super().__new__(cls)
         return OperateRedis._instance
+
     
-    def conn_redis(self):
+    def __conn_redis(self):
         """
         连接 Redis 数据库
             @return:
                 redis_connection
         """
-        if self.REDIS_CONFIG['AUTH'] is True:
+        if self.redis_conf.AUTH is True:
             pool = redis.ConnectionPool(
-                host=self.REDIS_CONFIG['HOST'],
-                port=self.REDIS_CONFIG['PORT'],
-                password=self.REDIS_CONFIG['PASSWORD'],
-                decode_responses=self.REDIS_CONFIG['DECODE_RESPONSES']
+                host=self.redis_conf.HOST,
+                port=self.redis_conf.PORT,
+                password=self.redis_conf.PASSWORD,
+                decode_responses=self.redis_conf.DECODE_RESPONSES
             )
         else:
             pool = redis.ConnectionPool(
-                host=self.REDIS_CONFIG['HOST'],
-                port=self.REDIS_CONFIG['PORT'],
-                decode_responses=self.REDIS_CONFIG['DECODE_RESPONSES']
+                host=self.redis_conf.HOST,
+                port=self.redis_conf.PORT,
+                decode_responses=self.redis_conf.DECODE_RESPONSES
             )
-        conn = redis.Redis(connection_pool=pool)
-        return conn
+        self.redis_cli = redis.Redis(connection_pool=pool, db=self.redis_conf.DEFAULT_DB)
+        logging.info("redis connected.")
+
+
+    def get_conn(self):
+        if not self.redis_cli:
+            self.__conn_redis()
+        return self.redis_cli
+
+
+    def conn_redis(self):
+        """兼容老代码
+        """
+        return self.get_conn()
+
+
+    def __del__(self):
+        try:
+            self.redis_cli.close()
+        except:
+            pass
+        logging.info("redis closed.")
