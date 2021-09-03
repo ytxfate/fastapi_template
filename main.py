8f347b2b916b769dbf43b964858412c2c6cfbb3c
hsd <1426482738@qq.com>
2021-09-03 11:07:37 +0800

    🎈 perf(日志记录): 日志添加记录时间

--- main.py
+++ main.py
@@ -7,13 +7,18 @@
 '''
 
 # Standard library imports
-
+from copy import deepcopy
 # Third party imports
 import uvicorn
+from uvicorn.config import LOGGING_CONFIG
 # Local application imports
 from project.config.sys_config import app_run_conf, isFormalSystem
 
 
+
+custom_logging_config = deepcopy(LOGGING_CONFIG)
+custom_logging_config['formatters']['default']['fmt'] = '%(asctime)s - ' + custom_logging_config['formatters']['default']['fmt']
+custom_logging_config['formatters']['access']['fmt'] = '%(asctime)s - ' + custom_logging_config['formatters']['access']['fmt']
 def main_run():
     if isFormalSystem is True:
         app_run_conf['RELOAD'] = False
@@ -23,7 +28,8 @@ def main_run():
                 port=app_run_conf['PORT'],
                 reload=app_run_conf['RELOAD'],
                 workers=app_run_conf['WORKERS'],
-                debug=app_run_conf['DEBUG'])
+                debug=app_run_conf['DEBUG'],
+                log_config=custom_logging_config)
 
 
 if __name__ == "__main__":
