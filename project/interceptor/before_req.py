5c606bc21dc66f0e53eca079fdfc5d1cf7aee13b
hsd <1426482738@qq.com>
2023-08-25 09:45:21 +0800

    🎈 perf(请求拦截器): 默认打开日志记录

--- project/interceptor/before_req.py
+++ project/interceptor/before_req.py
@@ -33,6 +33,6 @@ async def app_before_request(request: Request, call_next):
     # (取消注释 [文件头的库导入也需打开] 则生成日志信息,但并不保存,需自行选择存储方式)
     # 1 登录接口因为需要记录用户信息所以不能在此处记录
     # 2 下载文件的接口进行日志记录会导致服务阻塞
-    # if not(("/login" in path) or ('/download' in path)):
-    #     await sys_access_log(request=request)
+    if not(("/login" in path) or ('/download' in path)):
+        await sys_access_log(request=request)
     return await call_next(request)
