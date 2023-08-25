3e55d7df460007258aab342f02361420bc4357e3
hsd <1426482738@qq.com>
2023-08-25 09:40:14 +0800

    🐞 fix(请求拦截器): 文档路径修改

--- project/interceptor/before_req.py
+++ project/interceptor/before_req.py
@@ -16,6 +16,7 @@ from project.utils import resp_code
 from project.utils.comm_ret import comm_ret
 from project.utils.jwt_auth import JWTAuth
 from project.utils.sys_access_log import sys_access_log
+from project.config.sys_config import prefix_api_path
 
 
 logger = logging.getLogger("uvicorn")
@@ -24,7 +25,9 @@ logger = logging.getLogger("uvicorn")
 async def app_before_request(request: Request, call_next):
     path = request.url.path
     # 文档接口不拦截
-    if "/docs" == path or '/openapi.json' == path or '/redoc' == path:
+    if f"{prefix_api_path}/docs" == path or \
+        f'{prefix_api_path}/openapi.json' == path or \
+        f'{prefix_api_path}/redoc' == path:
         return await call_next(request)
     # 访问日志记录 
     # (取消注释 [文件头的库导入也需打开] 则生成日志信息,但并不保存,需自行选择存储方式)
