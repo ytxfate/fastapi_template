7a8249b00a1e584a789ade32710f3c5867c58dff
ytx <1426482738@qq.com>
2026-06-23 21:54:41 +0800

    🎈 perf(utils): MongoDB / Redis哨兵 连接工具优化

--- project/utils/operate_mongodb.py
+++ project/utils/operate_mongodb.py
@@ -1,43 +1,52 @@
 #!/usr/bin/env python3
 # -*- coding:utf-8 -*-
 
-'''
-@File :  operate_mongodb.py  
+"""
+@File :  operate_mongodb.py
 @Desc :  操作 MongoDB 数据库
-'''
+"""
 
-# Standard library imports
-import threading
-from collections import namedtuple
 import logging
-# Third party imports
-import pymongo
-# Local application imports
+import threading
+import time
+from collections import namedtuple
+from typing import Tuple
+
+from pymongo import MongoClient
+from pymongo.database import Database
+from pymongo.errors import ConnectionFailure
+
 from project.config.db_config import MONGODB_CONF, MONGODB_CONF_T
 from project.config.sys_config import isFormalSystem
 
 # ============================ 定义 MongoDB 连接配置 =========================== #
-MONGODB_CONF_NT = namedtuple("MONGODB_CONFIG", [
-    "URL",          # 有此项则优先用此项进行数据库连接
-                    # 否则用 HOST 和 PORT 连接
-    "HOST",         # 连接地址
-    "PORT",         # 连接端口
-    "AUTH",         # AUTH 为 True 时需要进行 用户认证
-    "USERNAME",     # 用户名
-    "PASSWORD",     # 密码
-    "DEFAULT_DB"    # 默认数据库
-])
+MONGODB_CONF_NT = namedtuple(
+    "MONGODB_CONFIG",
+    [
+        "URL",  # 有此项则优先用此项进行数据库连接
+        # 否则用 HOST 和 PORT 连接
+        "HOST",  # 连接地址
+        "PORT",  # 连接端口
+        "AUTH",  # AUTH 为 True 时需要进行 用户认证
+        "USERNAME",  # 用户名
+        "PASSWORD",  # 密码
+        "DEFAULT_DB",  # 默认数据库
+    ],
+)
 # 设置默认值
-MONGODB_CONF_NT.__new__.__defaults__ = ("",
-                                        "127.0.0.1",
-                                        27017,
-                                        False,
-                                        "xxx",
-                                        "xxx",
-                                        "xxx")
+MONGODB_CONF_NT.__new__.__defaults__ = (
+    "",
+    "127.0.0.1",
+    27017,
+    False,
+    "xxx",
+    "xxx",
+    "xxx",
+)
 # ============================================================================ #
 
 logger = logging.getLogger(__name__)
+
 
 class __OperateMongodb:
     """
@@ -45,96 +54,95 @@ class __OperateMongodb:
     此方法不实现单例模式, 以适应多库连接操作
     """
 
-    conn_mongo, db_mongo = None, None
-    
-    def __init__(self, priority_conf: dict={}):
-        """priority_conf 存在则优先使用
-        """
+    conn_mongo: MongoClient
+    db_mongo: Database
+
+    def __init__(self, priority_conf: dict = {}):
+        """priority_conf 存在则优先使用"""
         # 判断获取那个连接配置
         tmp_conf = priority_conf
         self.mongodb_conf = MONGODB_CONF_NT(**tmp_conf)
+        self.__conn_client()
+        self.__conn_database()
 
+    def __conn_client(self, enforce: bool = False):
+        if not hasattr(self, "conn_mongo") or enforce is True:
+            if self.mongodb_conf.URL:
+                self.conn_mongo = MongoClient(host=self.mongodb_conf.URL)
+            else:
+                self.conn_mongo = MongoClient(
+                    host=self.mongodb_conf.HOST,
+                    port=self.mongodb_conf.PORT,
+                )
+            logger.debug("mongodb: %s", self.mongodb_conf)
+            logger.info("mongodb connected(client)")
 
-    def __conn_mongodb(self):
-        """
-        连接 MongoDB 数据库
-            @return:
-                mongo_connection and mongo_database
-        """
-        if self.mongodb_conf.URL:
-            self.conn_mongo = pymongo.MongoClient(host=self.mongodb_conf.URL)
-        else:
-            self.conn_mongo = pymongo.MongoClient(host=self.mongodb_conf.HOST,
-                                                  port=self.mongodb_conf.PORT)
-        self.db_mongo = self.conn_mongo.get_database(self.mongodb_conf.DEFAULT_DB)
-        if self.mongodb_conf.AUTH is True:
-            self.db_mongo.authenticate(self.mongodb_conf.USERNAME,
-                                       self.mongodb_conf.PASSWORD)
-        logger.debug("mongodb connected: %s", self.mongodb_conf)
+    def __conn_database(self, enforce: bool = False):
+        if not hasattr(self, "db_mongo") or enforce is True:
+            self.db_mongo = self.conn_mongo.get_database(self.mongodb_conf.DEFAULT_DB)
+            if self.mongodb_conf.AUTH is True:
+                self.db_mongo.authenticate(
+                    self.mongodb_conf.USERNAME,
+                    self.mongodb_conf.PASSWORD,
+                )
+            logger.info("mongodb connected(database)")
 
+    def reconnect(self):
+        self.close()
+        self.__conn_client(True)
+        self.__conn_database(True)
 
-    def get_conn_and_db(self):
-        """获取 mongo_connection 和 mongo_database
-        """
-        if (not self.conn_mongo) or (not self.db_mongo):
-            self.__conn_mongodb()
+    def get_conn_and_db(self) -> Tuple[MongoClient, Database]:
+        """获取 mongo_connection 和 mongo_database"""
+        while 1:
+            try:
+                self.db_mongo.command("ping")
+                break
+            except ConnectionFailure:
+                time.sleep(1)
+                self.reconnect()
         return self.conn_mongo, self.db_mongo
 
+    def close(self):
+        try:
+            self.conn_mongo.close()
+        except Exception as e:
+            logger.warning(e)
+        logger.info("mongodb closed.")
 
-    def conn_mongodb(self):
-        """兼容老代码
-        """
+    def __del__(self):
+        self.close()
+
+    def conn_mongodb(self):  # 兼容老代码
         return self.get_conn_and_db()
 
 
-    def __del__(self):
-        try:
-            self.conn_mongo.close()
-        except:
-            pass
-        logger.info("mongodb closed.")
-
-
-
 class OperateMongodb(__OperateMongodb):
-    """MongoDB 数据库操作
-    """
+    """MongoDB 数据库操作"""
+
     _instance_lock = threading.Lock()
-    def __init__(self, priority_conf: dict={}):
-        """priority_conf 存在则优先使用
-        """
-        # 判断获取那个连接配置
-        tmp_conf = priority_conf or (MONGODB_CONF if isFormalSystem else MONGODB_CONF_T)
-        self.mongodb_conf = MONGODB_CONF_NT(**tmp_conf)
-    
+
+    def __init__(self, priority_conf: dict = {}):
+        """priority_conf 存在则优先使用"""
+        super().__init__(
+            priority_conf or (MONGODB_CONF if isFormalSystem else MONGODB_CONF_T)
+        )
+
     def __new__(cls, *args, **kwargs):
         """
         实现单例模式
         """
-        if not hasattr(cls, '_instance'):
+        if not hasattr(cls, "_instance"):
             with OperateMongodb._instance_lock:
-                if not hasattr(cls, '_instance'):
+                if not hasattr(cls, "_instance"):
                     OperateMongodb._instance = super().__new__(cls)
         return OperateMongodb._instance
 
 
-class OperateMongodbxxx(__OperateMongodb):
-    """MongoDB 数据库操作
-    """
-    _instance_lock = threading.Lock()
-    def __init__(self, priority_conf: dict={}):
-        """priority_conf 存在则优先使用
-        """
-        # 判断获取那个连接配置
-        tmp_conf = priority_conf or (MONGODB_CONF if isFormalSystem else MONGODB_CONF_T)
-        self.mongodb_conf = MONGODB_CONF_NT(**tmp_conf)
-    
-    def __new__(cls, *args, **kwargs):
-        """
-        实现单例模式
-        """
-        if not hasattr(cls, '_instance'):
-            with OperateMongodbxxx._instance_lock:
-                if not hasattr(cls, '_instance'):
-                    OperateMongodbxxx._instance = super().__new__(cls)
-        return OperateMongodbxxx._instance
+if __name__ == "__main__":
+    conn, db_mongo = OperateMongodb().get_conn_and_db()
+    print(conn, id(conn))
+    print(db_mongo["test"].find_one())
+    conn2, db_mongo2 = OperateMongodb().get_conn_and_db()
+    print(conn2, id(conn2))
+    print(db_mongo2["test"].find_one())
--- project/utils/opt_redis_sentinel.py
+++ project/utils/opt_redis_sentinel.py
@@ -1,59 +1,129 @@
 #!/usr/bin/env python3
 # -*- coding:utf-8 -*-
 
-'''
-@File :  opt_redis_sentinel.py  
+"""
+@File :  opt_redis_sentinel.py
 @Desc :  操作 Redis 哨兵数据库集群
-'''
+"""
 
-# Standard library imports
+import logging
 import threading
-# Third party imports
+from collections import namedtuple
+from typing import Tuple, Union
+
+from redis import Redis
+from redis.exceptions import ConnectionError, RedisError, TimeoutError
 from redis.sentinel import Sentinel
-# Local application imports
-from project.config.db_config import (REDIS_SENTINELS_CONFIG_T,
-                                      REDIS_SENTINELS_CONFIG)
+
+from project.config.db_config import REDIS_SENTINELS_CONFIG, REDIS_SENTINELS_CONFIG_T
 from project.config.sys_config import isFormalSystem
 
+logger = logging.getLogger(__name__)
 
-class OptRedisSentinel:
+# ============================= 定义连接配置 ============================ #
+REDIS_SENTINEL_CONF_NT = namedtuple(
+    "REDIS_SENTINEL_CONF",
+    [
+        "SENTINELS",  # ip端口集合
+        "SERVICE_NAME",  # 服务名称
+        "PASSWORD",  # 密码
+        "SENTINEL_PWD",  # sentinel 密码
+        "DECODE_RESPONSES",  # 解码
+        "DB",
+    ],
+)
+# 设置默认值
+REDIS_SENTINEL_CONF_NT.__new__.__defaults__ = (
+    [("127.0.0.1", 6379)],
+    "mymaster",
+    None,
+    None,
+    True,
+    0,
+)
+# ============================================================================ #
+
+
+class __OptRedisSentinel:
     """
     操作 Redis 哨兵数据库集群
     """
+
     _instance_lock = threading.Lock()
+    master: Redis
+    slave: Redis
 
-    def __init__(self):
-        # 根据 isFormalSystem 判断连接哪个 redis 数据库
-        self.REDIS_CONFIG = REDIS_SENTINELS_CONFIG if isFormalSystem else REDIS_SENTINELS_CONFIG_T
-        self.redis_master, self.redis_slave = None, None
-        self.conn_kwargs = {}
-        if self.REDIS_CONFIG['AUTH'] is True:
-            self.conn_kwargs['password'] = self.REDIS_CONFIG['PASSWORD']
+    def __init__(self, conf: Union[dict, REDIS_SENTINEL_CONF_NT]):
+        if isinstance(conf, REDIS_SENTINEL_CONF_NT):
+            self.conf = conf
+        else:
+            self.conf = REDIS_SENTINEL_CONF_NT(**conf)
 
+        self.conn_kwargs = {
+            "db": self.conf.DB,
+            "socket_timeout": 1,
+            "retry_on_timeout": True,
+            "health_check_interval": 10,
+            "socket_connect_timeout": 5,
+            "retry_on_error": [RedisError, ConnectionError, TimeoutError],
+        }
+        self.sentinel_kwargs = {
+            "socket_timeout": 1,
+            "retry_on_timeout": True,
+            "health_check_interval": 10,
+            "socket_connect_timeout": 5,
+            "retry_on_error": [RedisError, ConnectionError, TimeoutError],
+        }
+        if self.conf.PASSWORD:
+            self.conn_kwargs["password"] = self.conf.PASSWORD
+        if self.conf.SENTINEL_PWD:
+            self.sentinel_kwargs["password"] = self.conf.SENTINEL_PWD
+        logger.debug(self.conf)
 
     def __conn_redis(self):
-        sentinel = Sentinel(self.REDIS_CONFIG['SENTINELS'], socket_timeout=0.1,
-                            decode_responses=self.REDIS_CONFIG['DECODE_RESPONSES'],
-                            **self.conn_kwargs)
-        self.redis_master = sentinel.master_for(self.REDIS_CONFIG['SERVICE_NAME'],
-                                                socket_timeout=0.1)
-        self.redis_slave = sentinel.slave_for(self.REDIS_CONFIG['SERVICE_NAME'],
-                                              socket_timeout=0.1)
+        sentinel = Sentinel(
+            self.conf.SENTINELS,
+            sentinel_kwargs=self.sentinel_kwargs,
+            decode_responses=self.conf.DECODE_RESPONSES,
+            **self.conn_kwargs,
+        )
+        self.master = sentinel.master_for(
+            self.conf.SERVICE_NAME,
+        )
+        self.slave = sentinel.slave_for(
+            self.conf.SERVICE_NAME,
+        )
 
-    
-    def __new__(cls, *args, **kwargs):
-        if not hasattr(cls, '_instance'):
-            with OptRedisSentinel._instance_lock:
-                if not hasattr(cls, '_instance'):
-                    OptRedisSentinel._instance = super().__new__(cls)
-        return OptRedisSentinel._instance
-    
-    def conn_redis(self):
+    def conn_redis(self) -> Tuple[Redis, Redis]:
         """
         连接 Redis 数据库
             @return:
                 redis_master_connection, redis_slave_connection
         """
-        if not(self.redis_master or self.redis_slave):
+        if not hasattr(self, "master") or not hasattr(self, "slave"):
             self.__conn_redis()
-        return self.redis_master, self.redis_slave
+        return self.master, self.slave
+
+
+class OptRedisSentinel(__OptRedisSentinel):
+    """redis 哨兵连接工具
+
+    注:
+        实时性要求高的操作在 master 上执行读写操作
+        实时性要求低的操作在 master 上执行写操作, slave 上执行读操作
+    """
+
+    _instance_lock = threading.Lock()
+
+    def __init__(self):
+        super().__init__(
+            REDIS_SENTINELS_CONFIG if isFormalSystem else REDIS_SENTINELS_CONFIG_T
+        )
+
+    def __new__(cls, *args, **kwargs):
+        """单例"""
+        if not hasattr(cls, "_instance"):
+            with OptRedisSentinel._instance_lock:
+                if not hasattr(cls, "_instance"):
+                    OptRedisSentinel._instance = super().__new__(cls)
+        return OptRedisSentinel._instance
